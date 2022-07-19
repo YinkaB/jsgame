3a50aef837e0d149cd7f04e7ce28293079739754
YinkaB <52660217+YinkaB@users.noreply.github.com>
2022-07-19 13:41:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-alert("This is a rock,paper and scissors game run in the console. Are you ready?")
+ alert("This is a rock,paper and scissors game run in the console. Are you ready?")
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase()==="rock" && computerSelection[computerPlay]==="scissors" || playerSelection.toLowerCase()==="paper" && computerSelection[computerPlay]==="rock" || playerSelection.toLowerCase()==="scissors" && computerSelection[computerPlay]==="paper"){
+ function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase()=="rock" && computerSelection[computerPlay]=="scissors" || playerSelection.toLowerCase()=="paper" && computerSelection[computerPlay]=="rock" || playerSelection.toLowerCase()=="scissors" && computerSelection[computerPlay]=="paper"){
        playerScore++;
       return win;
     }
-    else if (playerSelection.toLowerCase()==="rock" && computerSelection[computerPlay]==="paper" || playerSelection.toLowerCase()==="scissors" && computerSelection[computerPlay]==="rock" || playerSelection.toLowerCase()==="paper" && computerSelection[computerPlay]==="scissors"){
+    else if (playerSelection.toLowerCase()=="rock" && computerSelection[computerPlay]=="paper" || playerSelection.toLowerCase()=="scissors" && computerSelection[computerPlay]=="rock" || playerSelection.toLowerCase()=="paper" && computerSelection[computerPlay]=="scissors"){
       computerScore++;
       return lose;
     }
-      else if (playerSelection.toLowerCase() === computerSelection[computerPlay]){
+      else if (playerSelection.toLowerCase() == computerSelection[computerPlay]){
      tieScore++;
       return tie;
-    } 
+    } //if player input rock compare to rock, its a tie
     else {
       return "you must input rock, paper or scissors";
     
-    } 
+    } //else if player input paper compare to rock, you win
     }
     
-      let playerSelection = prompt("lets play rock, paper, scissors"); 
-    console.log ("you choose " + playerSelection.toLowerCase()); //show user input in lowercase
+      let playerSelection = prompt("lets play rock papper scissors"); //define player, player input
+    //let player = playerSelection.toLowerCase(); //convert user input to lowercase *old
+    console.log ("you choose " +playerSelection.toLowerCase()); //show user input in lowercase
     
-    let computerSelection = ["rock","paper","scissors"];
-    let computerPlay= Math.floor(Math.random() * computerSelection.length); 
+    let computerSelection = ["rock","paper","scissors"];//declare computerSelection
+    let computerPlay= Math.floor(Math.random() * computerSelection.length);//declare computerPlay
     console.log("The computer choose " +computerPlay, computerSelection[computerPlay]);//define computer input
       
     //define scores
@@ -43,14 +44,13 @@ function playRound(playerSelection, computerSelection) {
     //loop
     function game() {
        
-     for (let i = 0; i <4; i++) {  
-        //what is going to be loop
-     let playerSelection = prompt("lets play rock papper scissors"); //define player, player input
+     for (let i = 0; i <4; i++) { //what is going to be loop
+     playerSelection = prompt("lets play rock papper scissors"); //define player, player input
     
     console.log ("you choose " +playerSelection.toLowerCase()); //show user input in lowercase
        
-     let computerSelection = ["rock","paper","scissors"];
-     let computerPlay= Math.floor(Math.random() * computerSelection.length); 
+     computerSelection = ["rock","paper","scissors"];//update computerSelection
+     computerPlay= Math.floor(Math.random() * computerSelection.length);//update computerPlay
        
      console.log("The computer choose " +computerPlay, computerSelection[computerPlay]);//define computer input  
     
@@ -71,7 +71,7 @@ function playRound(playerSelection, computerSelection) {
     
     console.log( "Your final score is " +playerScore +" wins " +computerScore +" loses and " +tieScore +" draws.\n -------");
     
-    let score=playerScore+computerScore+tieScore;
+    score=playerScore+computerScore+tieScore;
     
     console.log("You played this game " +score +" times.\n -------\nGame over.");
     //count win lose or tie
